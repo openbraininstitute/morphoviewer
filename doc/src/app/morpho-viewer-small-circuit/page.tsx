09160c5fab08ce73cc1163156d5cedd173e7057d
Tolokoban <contact@tolokoban.org>
2026-02-26 08:25:56 +0100

    SmallCircuit Click

--- doc/src/app/morpho-viewer-small-circuit/page.tsx
+++ doc/src/app/morpho-viewer-small-circuit/page.tsx
@@ -1,51 +1,65 @@
 import {
-	MorphoViewerSmallCircuit,
-	type MorphoViewerSmallCircuitCell,
-	type MorphoViewerSmallCircuitCellData,
-	morphoViewerConvertMorphologyIntoTree,
-} from "@bbp/morphoviewer";
-import React from "react";
-import { CIRCUIT } from "./data";
-import styles from "./page.module.css";
+  MorphoViewerSmallCircuit,
+  type MorphoViewerSmallCircuitCell,
+  type MorphoViewerSmallCircuitCellData,
+  morphoViewerConvertMorphologyIntoTree,
+} from '@bbp/morphoviewer';
+import React from 'react';
+
+import { CIRCUIT } from './data';
+
+import styles from './page.module.css';
 
 export default function Page() {
-	const [highlightedCellIds, setHighlightedCellIds] = React.useState([""]);
-	const handleCellHover = (
-		cell: MorphoViewerSmallCircuitCell | undefined,
-	): void => {
-		setHighlightedCellIds([cell?.id ?? ""]);
-	};
-	return (
-		<div className={styles.page}>
-			<div>
-				<MorphoViewerSmallCircuit
-					className={styles.viewer}
-					backgroundColor="#000"
-					circuit={CIRCUIT}
-					loadCell={loadCell}
-					onCellHover={handleCellHover}
-					highlightedCellIds={highlightedCellIds}
-				/>
-			</div>
-			<div>
-				<h1>&lt;MorphoViewerSmallCircuit /&gt;</h1>
-			</div>
-		</div>
-	);
+  const [selectedCells, setSelectedCells] = React.useState<string[]>([]);
+  const [highlightedCellId, setHighlightedCellId] = React.useState('');
+  const highlightedCellIds = React.useMemo(
+    () => [...selectedCells, highlightedCellId],
+    [selectedCells, highlightedCellId]
+  );
+  const handleCellHover = (cell: MorphoViewerSmallCircuitCell | undefined): void => {
+    setHighlightedCellId(cell?.id ?? '');
+  };
+  const handleCellClick = (cell: MorphoViewerSmallCircuitCell | undefined): void => {
+    if (!cell) return;
+
+    if (selectedCells.includes(cell.id)) {
+      setSelectedCells(selectedCells.filter((id) => id !== cell.id));
+    } else {
+      setSelectedCells([...selectedCells, cell.id]);
+    }
+  };
+
+  return (
+    <div className={styles.page}>
+      <div>
+        <MorphoViewerSmallCircuit
+          className={styles.viewer}
+          backgroundColor="#000"
+          circuit={CIRCUIT}
+          loadCell={loadCell}
+          onCellHover={handleCellHover}
+          onCellClick={handleCellClick}
+          highlightedCellIds={highlightedCellIds}
+        />
+      </div>
+      <div>
+        <h1>&lt;MorphoViewerSmallCircuit /&gt;</h1>
+      </div>
+    </div>
+  );
 }
 
-async function loadCell(
-	id: string,
-): Promise<MorphoViewerSmallCircuitCellData | null> {
-	try {
-		const resp = await fetch("./assets/morpho-02.json");
-		const morphology = await resp.json();
-		return {
-			type: "tree",
-			data: morphoViewerConvertMorphologyIntoTree(morphology, id),
-		};
-	} catch (error) {
-		console.error(`Unable to load cell "${id}":`, error);
-		return null;
-	}
+async function loadCell(id: string): Promise<MorphoViewerSmallCircuitCellData | null> {
+  try {
+    const resp = await fetch('./assets/morpho-02.json');
+    const morphology = await resp.json();
+    return {
+      type: 'tree',
+      data: morphoViewerConvertMorphologyIntoTree(morphology, id),
+    };
+  } catch (error) {
+    console.error(`Unable to load cell "${id}":`, error);
+    return null;
+  }
 }
--- doc/src/app/page.tsx
+++ doc/src/app/page.tsx
@@ -1,44 +1,38 @@
-import { ViewPanel } from "@tolokoban/ui";
+import { ViewPanel } from '@tolokoban/ui';
 
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Page() {
-	return (
-		<ViewPanel
-			className={styles.page}
-			fullwidth
-			position="absolute"
-			display="flex"
-			justifyContent="center"
-			alignItems="start"
-			gap="L"
-			color="neutral-5"
-			fullsize
-		>
-			<a href="#/morpho-viewer-simul">
-				<Comp name="MorphoViewerSimul" />
-			</a>
-			<a href="#/morpho-viewer-small-circuit">
-				<Comp name="MorphoViewerSmallCircuit" />
-			</a>
-			<a href="./docs/index.html">API Documentation</a>
-		</ViewPanel>
-	);
+  return (
+    <ViewPanel
+      className={styles.page}
+      fullwidth
+      position="absolute"
+      display="flex"
+      justifyContent="center"
+      alignItems="start"
+      gap="L"
+      color="neutral-5"
+      fullsize
+    >
+      <a href="#/morpho-viewer-simul">
+        <Comp name="MorphoViewerSimul" />
+      </a>
+      <a href="#/morpho-viewer-small-circuit">
+        <Comp name="MorphoViewerSmallCircuit" />
+      </a>
+      <a href="#/api">API Documentation</a>
+    </ViewPanel>
+  );
 }
 
-function Comp({
-	name,
-	attributes = {},
-}: {
-	name: string;
-	attributes?: Record<string, string>;
-}) {
-	const keys = Object.keys(attributes);
-	return (
-		<div>
-			<code>
-				&lt;<strong>{name}</strong> /&gt;
-			</code>
-		</div>
-	);
+function Comp({ name, attributes = {} }: { name: string; attributes?: Record<string, string> }) {
+  const keys = Object.keys(attributes);
+  return (
+    <div>
+      <code>
+        &lt;<strong>{name}</strong> /&gt;
+      </code>
+    </div>
+  );
 }
