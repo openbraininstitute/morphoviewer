332e1250c533c1ad47806653ee3e9b6caf646d34
Tolokoban <contact@tolokoban.org>
2026-03-06 16:07:20 +0100

    Add section list

--- lib/src/components/morpho-viewer-simul/components/hint/hint.tsx
+++ lib/src/components/morpho-viewer-simul/components/hint/hint.tsx
@@ -9,77 +9,91 @@ import type { StructureItem } from "../../painter/structure";
 import styles from "./hint.module.css";
 
 export interface HintProps {
-    className?: string;
-    painterManager: PainterManager;
+  className?: string;
+  painterManager: PainterManager;
 }
 
 export function HintPanel({ className, painterManager }: HintProps) {
-    const hovered = useEventValue(
-        { x: 0, y: 0, item: null, offset: 0 },
-        painterManager.eventHover,
-    );
-    const visible = useEventValue(false, painterManager.eventHintVisible);
+  const hovered = useEventValue({ x: 0, y: 0, item: null, offset: 0 }, painterManager.eventHover);
+  const visible = useEventValue(false, painterManager.eventHintVisible);
 
-    if (!visible || !hovered.item) {
-        return null;
-    }
+  if (!visible || !hovered.item) {
+    return null;
+  }
 
-    return (
-        <div
-            className={classNames(className, styles.hintPanel)}
-            key="hint"
-            style={hovered.y > 0 ? { bottom: "1em" } : { top: "4em" }}
-        >
-            <HintContent painterManager={painterManager} />
-        </div>
-    );
+  return (
+    <div
+      className={classNames(className, styles.hintPanel)}
+      key="hint"
+      style={hovered.y > 0 ? { bottom: "1em" } : { top: "4em" }}
+    >
+      <HintContent painterManager={painterManager} />
+    </div>
+  );
 }
 
 export function HintContent({ className, painterManager }: HintProps) {
-    const hovered = painterManager.hoverItem;
+  const hovered = painterManager.hoverItem;
 
-    if (!hovered.item) {
-        return null;
-    }
-    const { item, offset } = hovered;
+  if (!hovered.item) {
+    return null;
+  }
+  const { item, offset } = hovered;
 
-    return (
-        <div className={classNames(className, styles.hintContent)}>
-            <div>Section:</div>
-            <div>{resolveName(hovered.item)}</div>
-            <div>Section index:</div>
-            <div>{item.sectionIndex}</div>
-            <div>Segment index:</div>
-            <div>{item.segmentIndex}</div>
-            <div>Number of segments:</div>
-            <div>{item.segmentsCount}</div>
-            <div>Offset:</div>
-            <div>{offset.toFixed(3)}</div>
-            <div>Distance from soma:</div>
-            <div>
-                {(item.distanceFromSoma + offset * item.sectionLength).toFixed(
-                    2,
-                )} µm
-            </div>
-        </div>
-    );
+  return (
+    <div className={classNames(className, styles.hintContent)}>
+      <div>Section list:</div>
+      <div>{resolveSectionList(hovered.item)}</div>
+      <div>Section:</div>
+      <div>{resolveSectionName(hovered.item)}</div>
+      <div>Section index:</div>
+      <div>{item.sectionIndex}</div>
+      <div>Segment index:</div>
+      <div>{item.segmentIndex}</div>
+      <div>Number of segments:</div>
+      <div>{item.segmentsCount}</div>
+      <div>Offset:</div>
+      <div>{offset.toFixed(3)}</div>
+      <div>Distance from soma:</div>
+      <div>{(item.distanceFromSoma + offset * item.sectionLength).toFixed(2)} µm</div>
+    </div>
+  );
 }
 
-function resolveName(item: StructureItem): React.ReactNode {
-    switch (item.type) {
-        case MorphoViewerTreeItemType.Axon:
-            return "Axon";
-        case MorphoViewerTreeItemType.Soma:
-            return "Soma";
-        case MorphoViewerTreeItemType.Dendrite:
-            return "Dendrite";
-        case MorphoViewerTreeItemType.BasalDendrite:
-            return "Basal Dendrite";
-        case MorphoViewerTreeItemType.ApicalDendrite:
-            return "Apical dendrite";
-        case MorphoViewerTreeItemType.Myelin:
-            return "Myelin";
-        default:
-            return "Unknown";
-    }
+function resolveSectionName(item: StructureItem): React.ReactNode {
+  switch (item.type) {
+    case MorphoViewerTreeItemType.Axon:
+      return "Axon";
+    case MorphoViewerTreeItemType.Soma:
+      return "Soma";
+    case MorphoViewerTreeItemType.Dendrite:
+      return "Dendrite";
+    case MorphoViewerTreeItemType.BasalDendrite:
+      return "Basal Dendrite";
+    case MorphoViewerTreeItemType.ApicalDendrite:
+      return "Apical dendrite";
+    case MorphoViewerTreeItemType.Myelin:
+      return "Myelin";
+    default:
+      return "Unknown";
+  }
+}
+
+function resolveSectionList(item: StructureItem): React.ReactNode {
+  switch (item.type) {
+    case MorphoViewerTreeItemType.Axon:
+      return "Axonal";
+    case MorphoViewerTreeItemType.Soma:
+      return "Somatic";
+    case MorphoViewerTreeItemType.Dendrite:
+      return "Basal";
+    case MorphoViewerTreeItemType.BasalDendrite:
+      return "Basal";
+    case MorphoViewerTreeItemType.ApicalDendrite:
+      return "Apical";
+    case MorphoViewerTreeItemType.Myelin:
+      return "Myelinated";
+    default:
+      return resolveSectionName(item);
+  }
 }
