142db71633f5b54719a373438b73028d2a94c77c
Tolokoban <contact@tolokoban.org>
2026-05-04 18:08:36 +0200

    Embedded controls (#17)
    * Develop merge
    
    * Added a configurable controls layout that we used in Small Circuit
    
    * v0.24.22
    
    * Fixed bad merging

--- doc/src/app/morpho-viewer-octree/page.tsx
+++ doc/src/app/morpho-viewer-octree/page.tsx
@@ -1,4 +1,7 @@
-import { MorphoViewerOctree, type MorphoViewerOctreeProps } from "@openbraininstitute/morphoviewer";
+import {
+	MorphoViewerOctree,
+	type MorphoViewerOctreeProps,
+} from "@openbraininstitute/morphoviewer";
 import { assertType } from "@tolokoban/type-guards";
 import { ViewLabel, ViewOptions } from "@tolokoban/ui";
 import React from "react";
@@ -8,66 +11,70 @@ import { GizmoSettings } from "@/components/gizmo-settings";
 import Styles from "./page.module.css";
 
 export default function PageMorphoViewerOctree() {
-  const [meshId, setMeshId] = React.useState("1");
-  const [gizmo, setGizmo] = React.useState<boolean | MorphoViewerOctreeProps["gizmo"]>(true);
+	const [meshId, setMeshId] = React.useState("1");
+	const [gizmo, setGizmo] = React.useState<
+		boolean | MorphoViewerOctreeProps["gizmo"]
+	>(true);
 
-  return (
-    <div className={Styles.morphoViewerOctree}>
-      <header>
-        <ViewLabel>Choose the mesh:</ViewLabel>
-        <ViewOptions value={meshId} onChange={setMeshId}>
-          <div key="1">Example #1</div>
-          <div key="2">Example #2</div>
-        </ViewOptions>
-        <GizmoSettings value={gizmo} onChange={setGizmo} />
-      </header>
-      <MorphoViewerOctree
-        className={Styles.octree}
-        meshId={meshId}
-        gizmo={gizmo}
-        loadInfo={async (meshId: string) => {
-          const url = `./assets/octree/${meshId}/lod.json`;
-          console.debug("Loading info:", url);
-          const resp = await fetch(url);
-          if (!resp.ok) {
-            throw new Error(
-              `Unable to get info file: ${url}!\nError #${resp.status}: ${resp.statusText}`
-            );
-          }
-          const data = await resp.json();
-          assertType(data, {
-            bbox: {
-              min: ["array", "number"],
-              max: ["array", "number"],
-            },
-            files: ["array", "string"],
-          });
-          return {
-            bbox: data.bbox as BBox,
-            blockIds: data.files.map((filename) => filename.split(".")[0]),
-          };
-        }}
-        loadBlock={async (meshId: string, blockId: string) => {
-          const url = `./assets/octree/${meshId}/${blockId}.glb`;
-          console.debug("Loading:", url);
-          const resp = await fetch(url);
-          if (!resp.ok) {
-            console.error(
-              `Unable to get info file: ${url}!\nError #${resp.status}: ${resp.statusText}`
-            );
-            return null;
-          }
-          return {
-            type: "glb",
-            data: await resp.arrayBuffer(),
-          };
-        }}
-      />
-    </div>
-  );
+	return (
+		<div className={Styles.morphoViewerOctree}>
+			<header>
+				<ViewLabel>Choose the mesh:</ViewLabel>
+				<ViewOptions value={meshId} onChange={setMeshId}>
+					<div key="1">Example #1</div>
+					<div key="2">Example #2</div>
+				</ViewOptions>
+				<GizmoSettings value={gizmo} onChange={setGizmo} />
+			</header>
+			<MorphoViewerOctree
+				className={Styles.octree}
+				meshId={meshId}
+				gizmo={gizmo}
+				loadInfo={async (meshId: string) => {
+					const url = `./assets/octree/${meshId}/lod.json`;
+					console.debug("Loading info:", url);
+					const resp = await fetch(url);
+					if (!resp.ok) {
+						throw new Error(
+							`Unable to get info file: ${url}!\nError #${resp.status}: ${resp.statusText}`,
+						);
+					}
+					const data = await resp.json();
+					assertType(data, {
+						bbox: {
+							min: ["array", "number"],
+							max: ["array", "number"],
+						},
+						files: ["array", "string"],
+					});
+					return {
+						bbox: data.bbox as BBox,
+						blockIds: data.files.map(
+							(filename) => filename.split(".")[0],
+						),
+					};
+				}}
+				loadBlock={async (meshId: string, blockId: string) => {
+					const url = `./assets/octree/${meshId}/${blockId}.glb`;
+					console.debug("Loading:", url);
+					const resp = await fetch(url);
+					if (!resp.ok) {
+						console.error(
+							`Unable to get info file: ${url}!\nError #${resp.status}: ${resp.statusText}`,
+						);
+						return null;
+					}
+					return {
+						type: "glb",
+						data: await resp.arrayBuffer(),
+					};
+				}}
+			/>
+		</div>
+	);
 }
 
 interface BBox {
-  min: [number, number, number];
-  max: [number, number, number];
+	min: [number, number, number];
+	max: [number, number, number];
 }
--- lib/src/components/morpho-viewer-small-circuit/types.ts
+++ lib/src/components/morpho-viewer-small-circuit/types.ts
@@ -1,49 +1,63 @@
-import { TgdPainterGizmoOptions } from "@tolokoban/tgd";
-
-import type { MorphoViewerTree } from "../morpho-viewer-simul";
+import { TgdPainterGizmoOptions } from "@tolokoban/tgd"
+import { ControlsLayoutProps } from "../controls-layer/controls-layout"
+import type { MorphoViewerTree } from "../morpho-viewer-simul"
 
 export interface MorphoViewerSmallCircuitCell {
-  id: string;
-  center: [x: number, y: number, z: number];
-  orientation: [x: number, y: number, z: number, w: number];
-  somaRadius: number;
-  color?: string;
+    id: string
+    center: [x: number, y: number, z: number]
+    orientation: [x: number, y: number, z: number, w: number]
+    somaRadius: number
+    color?: string
 }
 
 export type MorphoViewerSmallCircuitCellData = {
-  type: "tree";
-  data: MorphoViewerTree;
-};
+    type: "tree"
+    data: MorphoViewerTree
+}
 
 export interface MorphoViewerSmallCircuitProps {
-  className?: string;
-  backgroundColor?: string;
-  circuit: MorphoViewerSmallCircuitCell[];
-  highlightedCellIds?: string[];
-  onCellHover?(cell: MorphoViewerSmallCircuitCell | undefined): void;
-  onCellClick?(cell: MorphoViewerSmallCircuitCell | undefined): void;
-  onClose?(): void;
-  /**
-   * A function to load a cell.
-   * @param id Unique identifier of the cell from attribute `circuit`.
-   */
-  loadCell(id: string): Promise<MorphoViewerSmallCircuitCellData | null>;
-  /**
-   * This callback is called when the loading starts (with a value of __0__),
-   * then every time a cell is loaded.
-   *
-   * @param progress Percentage of loaded cells so far. Between `0.0` and `1.0`.
-   */
-  onLoadProgress?(progress: number): void;
-  /**
-   * Display the axes controller gizmo.
-   * - `false`: do not show the Gizmo
-   * - `true`: show it with default options
-   * - `Partial<object>`:
-   *   - `alignX`: -1 meand left and +1 means right
-   *   - `alignY`: -1 meand bottom and +1 means top
-   *   - `size`: size of the Gizmo side in pixels
-   *   - `margin`: margin from the borders of the viewer (in pixels)
-   */
-  gizmo?: boolean | TgdPainterGizmoOptions;
+    className?: string
+    backgroundColor?: string
+    circuit: MorphoViewerSmallCircuitCell[]
+    /**
+     * Ids of the cells we want to highlight.
+     */
+    highlightedCellIds?: string[]
+    onCellHover?(cell: MorphoViewerSmallCircuitCell | undefined): void
+    onCellClick?(cell: MorphoViewerSmallCircuitCell | undefined): void
+    onClose?(): void
+    onMinimize?(): void
+    /**
+     * A function to load a cell.
+     * @param id Unique identifier of the cell from attribute `circuit`.
+     */
+    loadCell(id: string): Promise<MorphoViewerSmallCircuitCellData | null>
+    /**
+     * This callback is called when the loading starts (with a value of __0__),
+     * then every time a cell is loaded.
+     *
+     * @param progress Percentage of loaded cells so far. Between `0.0` and `1.0`.
+     */
+    onLoadProgress?(progress: number): void
+    /**
+     * Display the axes controller gizmo.
+     * - `false`: do not show the Gizmo
+     * - `true`: show it with default options
+     * - `Partial<object>`:
+     *   - `alignX`: -1 meand left and +1 means right
+     *   - `alignY`: -1 meand bottom and +1 means top
+     *   - `size`: size of the Gizmo side in pixels
+     *   - `margin`: margin from the borders of the viewer (in pixels)
+     */
+    gizmo?: boolean | TgdPainterGizmoOptions
+    /**
+     * Defines the control that are embedded with the viewer,
+     * in the header.
+     * 
+     * It's a list of blocks that are align with a wrappable flex layout and space between justification.
+     * If a block is an array, then its content is wrapped in a `<div></div>`.
+     * 
+     * Common actions can be defined by a name. Example: `reset-camera`, `fullscreen`, `minimize`, `close`.
+     */
+    controls?: ControlsLayoutProps["content"]
 }
